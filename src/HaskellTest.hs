7fda0147c2059b31c455dfd7bcf207d3e142b9bb
avatar-lavventura <alper.alimoglu@gmail.com>
2025-02-05 15:31:04 +0300

    Fix some bugs, minor improvements, and more

---
+++ src/HaskellTest.hs
@@ -0,0 +1,11 @@
+module HaskellTest where
+
+fib :: Integer -> Integer
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n - 1) + fib (n - 2)
+
+main :: IO ()
+main = do
+  let fibs = map fib [0..9]
+  print fibs
